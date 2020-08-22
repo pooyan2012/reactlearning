46ed46cbb69bceeef483f700cfc3e4f10e0e9848
pooyan <permangenat_pooyan@yahoo.com>
2020-08-22 12:45:16 +0430

    1st commit

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,5 @@
-console.log("app is running!");
+//JSX - JsvsScript XML
+var template = <p>This is JSX from app.js</p>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
