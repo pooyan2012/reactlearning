ca4ede5e9070e84df27b444ca46283d50e62027f
pooyan2012 <you@example.com>
2020-09-15 00:33:33 +0430

    2. Organizing Redux

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,8 +1,25 @@
-//15. Sorting Redux Data
+//2. Organizing Redux
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water bill" }));
+store.dispatch(addExpense({ description: "Gas bill" }));
+store.dispatch(setTextFilter("bill"));
+
+const state = store.getState;
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+store.dispatch(visibleExpenses);
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
