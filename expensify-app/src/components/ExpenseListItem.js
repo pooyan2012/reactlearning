44dbf207f195eb08cbbf795f7de6c0d99cbb0206
pooyan <permangenat_pooyan@yahoo.com>
2020-09-16 15:53:59 +0430

    7. Dropdown for Picking SortBy

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -2,11 +2,11 @@ import React from "react";
 import { connect } from "react-redux";
 import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <h3>{description}</h3>
     <p>
-      {amount} - {createAt}
+      {amount} - {createdAt}
     </p>
     <button
       onClick={() => {
