e34af77e6a0cf177d231c8dc9f034c0e1048084d
pooyan2012 <you@example.com>
2020-10-02 00:59:28 +0330

    5. Creating a Separate Test Database

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,12 +1,12 @@
-//4. Testing Async Redux Actions Part II
+//5. Creating a Separate Test Database
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+// import { addExpense } from './actions/expenses';
+// import { setTextFilter } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7thhlNS7KEfw6uf7tqfXmgQchzeaxeic",
-  authDomain: "expensify-1111.firebaseapp.com",
-  databaseURL: "https://expensify-1111.firebaseio.com",
-  projectId: "expensify-1111",
-  storageBucket: "expensify-1111.appspot.com",
-  messagingSenderId: "774359028220",
-  appId: "1:774359028220:web:ac24ba850b89845197a279",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 // Initialize Firebase
 try {
